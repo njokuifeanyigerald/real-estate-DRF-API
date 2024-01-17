fb61ca2c733bc8e0c3cf78f3e0e30bff87f1b1a0
njokuifeanyigerald <brainboyrichmond@gmail.com>
2024-01-17 13:12:14 +0100

    ALLOWED_HOSTS

--- real_estate/settings.py
+++ real_estate/settings.py
@@ -21,9 +21,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'os.environ.getwdkjwqw&#&*@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['https://drf-realestate.onrender.com', 'localhost']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost','https://drf-realestate.onrender.com']
 
 
 # Application definition
