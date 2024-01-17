82636e333dc9052b074be1a5849c85a79a584374
njokuifeanyigerald <brainboyrichmond@gmail.com>
2024-01-17 12:33:07 +0100

    added some render dependencies

--- real_estate/settings.py
+++ real_estate/settings.py
@@ -15,11 +15,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'bjaSJaiu237238^@%!mdefsaj,.??wdyqd215'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['127.0.0.1','https://drf-realestate.onrender.com', 'localhost']
 
 
 # Application definition
