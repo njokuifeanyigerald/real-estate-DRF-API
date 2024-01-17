f1d8cbfd38bac6d927e6552552f97575ace7fd8e
njokuifeanyigerald <brainboyrichmond@gmail.com>
2024-01-17 13:05:02 +0100

    BASEDIR edit

--- real_estate/settings.py
+++ real_estate/settings.py
@@ -3,7 +3,9 @@ import os
 import datetime
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = Path(__file__).resolve().parent.parent
+
 
 # env = environ.Env(
 #     # set casting, default value
