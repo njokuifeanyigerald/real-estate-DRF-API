03b65e1f151888b4bc79f7fc6c5431845392c40f
njokuifeanyigerald <brainboyrichmond@gmail.com>
2024-01-17 12:48:50 +0100

    made edits to the README.md file

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 set -o errexit
 
 pip install -r requirements.txt
-pip install --no-deps ruamel.yaml
+# pip install --no-deps ruamel.yaml
 
 python manage.py collectstatic --no-input
 python manage.py migrate
