28dbc7f441f24ebfa5a8419d9494f938dd4a8671
njokuifeanyigerald <brainboyrichmond@gmail.com>
2024-01-17 13:17:14 +0100

    changed debug to False anf then added env for secret key

--- real_estate/settings.py
+++ real_estate/settings.py
@@ -17,11 +17,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = os.environ.get('SECRET_KEY')
-SECRET_KEY = 'os.environ.getwdkjwqw&#&*@'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost','drf-realestate.onrender.com']
 
